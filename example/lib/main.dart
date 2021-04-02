2eed756a81487ccb4570e71590591ede9d7c8095
章博 <heybozai@sina.com>
2021-04-02 19:37:33 +0800

    适配新 sdk

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,12 +24,7 @@ class HomeRoute extends StatefulWidget {
 }
 
 class _HomeState extends State<HomeRoute> {
-  var permissions = [
-    Permission.location,
-    Permission.camera,
-    Permission.storage,
-    Permission.phone
-  ];
+  var permissions = [Permission.storage, Permission.phone];
   var locationState = "";
   var locationStateGeo = "";
 
@@ -56,16 +51,7 @@ class _HomeState extends State<HomeRoute> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               _buildConfigWidget(),
-              _buildSyncImmediateWidget(),
-              _buildSyncListImmediateWidget(),
-              _buildSyncWidget(),
-              _buildBindUserWidget(),
-              _buildUnBindUserWidget(),
-              _buildStopSDKWidget(),
-              _buildStartLocationWidget(),
-              _buildGeoStartLocationWidget(),
-              _buildStartMonitorWidget(),
-              _buildStopMonitorWidget()
+              _buildLoggingWidget(),
             ],
           ),
         ),
@@ -77,145 +63,18 @@ class _HomeState extends State<HomeRoute> {
     return ElevatedButton(
       child: Text("设置配置"),
       onPressed: () async {
-        /// 配置方法一
-        FTMobileAgentFlutter.configX(
-            Config("http://10.100.64.106:19457/v1/write/metrics")
-                .setAK("accid", "accsk")
-                .setDataKit("flutter_datakit")
-                .setEnableLog(true)
-                .setNeedBindUser(false)
-                .setToken("tkn_4c4f9f29f39c493199bb5abe7df6af21")
-                .setGeoKey(true, "46f60b8b6963de515749001b92a866c0")
-                .setMonitorType(MonitorType.BATTERY |
-                    MonitorType.NETWORK |
-                    MonitorType.LOCATION |
-                    MonitorType.GPU));
-
-        /// 配置方法二
-        /**FTMobileAgentFlutter.config(
-            "http://10.100.64.106:19457/v1/write/metrics",
-            akId: "accid",
-            akSecret: "accsk",
-            dataKitUUID: "flutter_datakit",
-            enableLog: true,
-            needBindUser: false,
-            monitorType: MonitorType.ALL);*/
+        ///配置
+        FTMobileAgentFlutter.config(
+            "http://172.16.5.9:9529");
       },
     );
   }
 
-  Widget _buildSyncImmediateWidget() {
+  Widget _buildLoggingWidget() {
     return ElevatedButton(
-      child: Text("同步一条数据（直接上传）"),
-      onPressed: () async {
-        var result = await FTMobileAgentFlutter.track(
-            "flutter_list_test", {"platform": "flutter"}, {"method": "直接同步"});
-        print("request success: $result");
-      },
-    );
-  }
-
-  Widget _buildSyncListImmediateWidget() {
-    return ElevatedButton(
-      child: Text("同步一组数据（直接上传）"),
-      onPressed: () async {
-        var result = await FTMobileAgentFlutter.trackList([
-          TrackBean("flutter_list_test", {"platform": "flutter"}),
-          TrackBean("flutter_list_test", {"platform": "flutter"},
-              tags: {"method": "直接同步"}),
-        ]);
-        print("request success: $result");
-      },
-    );
-  }
-
-  Widget _buildSyncWidget() {
-    return ElevatedButton(
-      child: Text("同步（后台执行）"),
+      child: Text("日志输出"),
       onPressed: () {
-        FTMobileAgentFlutter.trackBackground(
-            "flutter_list_test", {"method": "后台同步"},
-            tags: {"platform": "flutter"});
-      },
-    );
-  }
-
-  Widget _buildBindUserWidget() {
-    return ElevatedButton(
-      child: Text("绑定用户"),
-      onPressed: () {
-        FTMobileAgentFlutter.bindUser("flutter_demo", "id_001",
-            extras: {"platform": "flutter"});
-      },
-    );
-  }
-
-  Widget _buildUnBindUserWidget() {
-    return ElevatedButton(
-      child: Text("解绑用户"),
-      onPressed: () {
-        FTMobileAgentFlutter.unbindUser();
-      },
-    );
-  }
-
-  Widget _buildStopSDKWidget() {
-    return ElevatedButton(
-      child: Text("停止正在执行的操作"),
-      onPressed: () {
-        FTMobileAgentFlutter.stopSDK();
-      },
-    );
-  }
-
-  Widget _buildStartMonitorWidget() {
-    return RaisedButton(
-      child: Text("开启监控项周期上传"),
-      onPressed: () {
-        FTMobileAgentFlutter.startMonitor(MonitorType.ALL,
-            geoKey: "46f60b8b6963de515749001b92a866c0",
-            useGeoKey: true,
-            period: 10);
-      },
-    );
-  }
-
-  Widget _buildStopMonitorWidget() {
-    return RaisedButton(
-      child: Text("停止监控项周期上传"),
-      onPressed: () {
-        FTMobileAgentFlutter.stopMonitor();
-      },
-    );
-  }
-
-  Widget _buildStartLocationWidget() {
-    return ElevatedButton(
-      child: Text("定位异步通知结果" + locationState),
-      onPressed: () async {
-        var result = await FTMobileAgentFlutter.startLocation();
-        if (result != null) {
-          setState(() {
-            locationState =
-                "-code:${result['code']},message:${result['message']}";
-          });
-        }
-      },
-    );
-  }
-
-  Widget _buildGeoStartLocationWidget() {
-    return ElevatedButton(
-      child: Text("（仅 Android ）高德定位异步通知结果" + locationStateGeo),
-      onPressed: () async {
-        var result = await FTMobileAgentFlutter.startLocation(
-            geoKey: "46f60b8b6963de515749001b92a866c0");
-        if (result != null) {
-          setState(() {
-            locationStateGeo =
-                "-code:${result['code']},message:${result['message']}";
-          });
-        }
+        FTMobileAgentFlutter.logging("log content", FTLogStatus.info);
       },
     );
   }
--- ios/Classes/SwiftAgentPlugin.swift
+++ ios/Classes/SwiftAgentPlugin.swift
@@ -5,15 +5,8 @@ import FTMobileSDK
 public class SwiftAgentPlugin: NSObject, FlutterPlugin {
 
     static let METHOD_CONFIG = "ftConfig"
-    static let METHOD_TRACK = "ftTrack"
-    static let METHOD_TRACK_LIST = "ftTrackList"
-    static let METHOD_BIND_USER = "ftBindUser"
-    static let METHOD_UNBIND_USER = "ftUnBindUser"
-    static let METHOD_STOP_SDK = "ftStopSdk"
-    static let METHOD_TRACK_BACKGROUND = "ftTrackBackground"
-    static let METHOD_START_LOCATION  = "ftStartLocation"
-    static let METHOD_START_MONITOR = "ftStartMonitor"
-    static let METHOD_STOP_MONITOR = "ftStopMonitor"
+    static let METHOD_LOGGING = "ftLogging"
+
 
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "ft_mobile_agent_flutter", binaryMessenger: registrar.messenger())
@@ -29,181 +22,21 @@ public class SwiftAgentPlugin: NSObject, FlutterPlugin {
         }
         
         if (call.method == SwiftAgentPlugin.METHOD_CONFIG) {
-            self.ftConfig(metricsUrl: args["serverUrl"] as! String, akId: args["akId"] as? String, akSecret:(args["akSecret"] as? String),datakitUUID: args["datakitUUID"] as? String, enableLog: args["enableLog"] as? Bool, needBindUser: (args["needBindUser"] as? Bool),monitorType: (args["monitorType"] as? Int),datawayToken:(args["token"] as? String) )
+            let config: FTMobileConfig = FTMobileConfig(metricsUrl: args["metricsUrl"] as! String)
+            config.enableSDKDebugLog = true
+            FTMobileAgent.start(withConfigOptions: config)
             result(nil)
-        } else if (call.method == SwiftAgentPlugin.METHOD_TRACK) {
-            DispatchQueue.global(qos: .userInitiated).async {
-                
-                let resultDic = self.ftTrack(measurement: args["measurement"] as! String, tags: args["tags"] as? Dictionary<String, Any>, fields: args["fields"] as! Dictionary<String, Any>)
-                DispatchQueue.main.async {
-                    result(resultDic)
-                }
-            }
+        } else if (call.method == SwiftAgentPlugin.METHOD_LOGGING) {
+            let content = args["content"] as! String
+            let status = args["status"] as! Int
             
-        }else if(call.method == SwiftAgentPlugin.METHOD_TRACK_LIST){
+            FTMobileAgent.sharedInstance().logging(content,status:FTStatus.init(rawValue: status)!)
             
-            let list = args["list"] as! Array<Dictionary<String,Any?>>
-            DispatchQueue.global(qos: .userInitiated).async {
-                let resultDic = self.ftTrackList(items: list)
-                DispatchQueue.main.async {
-                    result(resultDic)
-                }
-            }
-        }else if(call.method == SwiftAgentPlugin.METHOD_BIND_USER){
-            self.ftBindUser(name: args["name"] as! String, id: args["id"] as! String, extras: args["extras"] as? Dictionary<String, Any>)
-            result(nil)
-        }else if(call.method == SwiftAgentPlugin.METHOD_TRACK_BACKGROUND){
-            self.ftTrackBackground(measurement: args["measurement"] as! String, tags: args["tags"] as? Dictionary<String, Any>, fields: (args["fields"] as! Dictionary<String, Any>))
-            result(nil)
-        }else if(call.method == SwiftAgentPlugin.METHOD_START_LOCATION){
-            FTMobileAgent.startLocation { (code, message) in
-                var resultDic = Dictionary<String,Any>()
-                resultDic = ["code":code,"response":message]
-                result(resultDic)
-            }
             
-        }else if(call.method == SwiftAgentPlugin.METHOD_STOP_SDK){
-            self.ftStopSdk()
-            result(nil)
-        }else if(call.method == SwiftAgentPlugin.METHOD_UNBIND_USER){
-            self.ftUnBindUser()
-            result(nil)
-        }else if(call.method == SwiftAgentPlugin.METHOD_START_MONITOR){
-            self.ftStartMonitor(period: args["period"] as? Int, monitorType: args["monitorType"] as? Int)
-            result(nil)
-        }else if(call.method == SwiftAgentPlugin.METHOD_STOP_MONITOR){
-            self.ftStopMonitor()
-            result(nil)
         }else{
             result(FlutterMethodNotImplemented)
+            
         }
-    }
-
-    
-    /// 设置条件
-    /// - Parameters:
-    ///   - metricsUrl: 服务器地址
-    ///   - akId: access key
-    ///   - akSecret: access secret
-    private func ftConfig(metricsUrl:String,akId:String?,akSecret:String?,datakitUUID:String?,enableLog:Bool?,needBindUser:Bool?,monitorType:Int?,datawayToken:String?) {
-        let enableRequestSigning = akId != nil && akSecret != nil
-        let config: FTMobileConfig = FTMobileConfig(metricsUrl: metricsUrl, datawayToken:datawayToken,  akId: akId, akSecret: akSecret, enableRequestSigning: enableRequestSigning)
-        
-//        config.enableAutoTrack = true
-        config.enableLog = true
-        config.needBindUser = needBindUser ?? false
-        if((monitorType) != nil){
-            config.monitorInfoType = FTMonitorInfoType(rawValue: UInt(monitorType!));
-        }
-        if(datakitUUID != nil){
-          config.xDataKitUUID = datakitUUID!
-        }
-        FTMobileAgent.start(withConfigOptions: config)
-
-    }
-    
-    
-    /// 上报数据
-    /// - Parameters:
-    ///   - measurement: 指标类型
-    ///   - tags: 标签
-    ///   - fields: 值
-    private func ftTrack(measurement:String,tags:Dictionary<String, Any>?,fields:Dictionary<String, Any>) ->Dictionary<String,Any>{
-        
-        let group = DispatchGroup()
-        var result:Dictionary<String,Any>?=nil;
-        group.enter()
-        if(tags != nil){
-            FTMobileAgent.sharedInstance().trackImmediate(measurement, tags: tags!,field: fields){
-              (code,response) -> () in
-            result = ["code":code,"response":response]
-            group.leave()
-             
-          }
-        }else{
-          FTMobileAgent.sharedInstance().trackImmediate(measurement,field: fields){
-              (code,response) -> () in
-            result = ["code":code,"response":response]
-            group.leave()
-          }
-        }
-        group.wait()
-        return result!
-    }
-    
-    private func ftTrackList(items:Array<Dictionary<String,Any?>>) ->Dictionary<String,Any>{
-        
-        
-        let group = DispatchGroup()
-        var result:Dictionary<String,Any>?=nil;
-        group.enter()
-        
-        let beans:NSMutableArray = []
-        for dic in items{
-            let bean = FTTrackBean()
-            bean.measurement = dic["measurement"] as! String
-            if((dic["tags"] as? Dictionary<String,Any?>)  != nil){
-                bean.tags = dic["tags"] as! Dictionary
-            }
-
-            bean.field = dic["fields"] as! Dictionary
-            beans.add(bean)
-
-        }
-        
-        FTMobileAgent.sharedInstance().trackImmediateList(beans as! [FTTrackBean]){
-                     (code,response) -> () in
-            result = ["code":code,"response":response]
-                   group.leave()
-                    
-                 }
-        group.wait()
-        
-        return result!
-    }
-
-    /// 主动埋点后台上传
-    /// - Parameters:
-    ///   - measurement:指标类型
-    ///   - tags:标签
-    ///   - fields: 值
-    private func ftTrackBackground(measurement:String,tags:Dictionary<String, Any>?,fields:Dictionary<String, Any>){
-        if(tags != nil){
-            FTMobileAgent.sharedInstance().trackBackground(measurement,tags:tags,field:fields)
-        }else{
-            FTMobileAgent.sharedInstance().trackBackground(measurement,field:fields)
-        }
-    }
-    /// 绑定用户
-    /// - Parameters:
-    ///   - name:用户名
-    ///   - id:用户id
-    ///   - extras:用户其他信息
-    private func ftBindUser(name:String,id:String,extras:Dictionary<String, Any>?){
-        FTMobileAgent.sharedInstance().bindUser(withName: name,id:id,exts:extras)
-    }
-    /// 启动监控项周期上传
-    private func ftStartMonitor(period:Int?,monitorType:Int?){
-        if((monitorType) != nil){
-            FTMobileAgent.sharedInstance().startMonitorFlush(withInterval: period ?? 10, monitorType: FTMonitorInfoType(rawValue: UInt(monitorType!)))
-        }else{
-            FTMobileAgent.sharedInstance().setMonitorFlushInterval(period ?? 10)
-            FTMobileAgent.sharedInstance().stopMonitorFlush()
-        }
-    }
-    /// 关闭监控项周期上传
-    private func ftStopMonitor(){
-        FTMobileAgent.sharedInstance().stopMonitorFlush()
-    }
-
-    /// 用户登出
-    private func ftUnBindUser(){
-        FTMobileAgent.sharedInstance().logout()
-    }
-    
-    /// 停止SDK后台正在执行的操作
-    private func ftStopSdk(){
-        FTMobileAgent.sharedInstance().resetInstance()
     }
     
 }
