b64bbb6194a43f291a607fbc5a13c5af14316bc8
章博 <brandonfaustine@gmail.com>
2023-08-07 15:43:47 +0800

    移除错误依赖

--- android/src/main/kotlin/com/cloudcare/ft/mobile/sdk/tracker/flutter/FTMobileAgentFlutter.kt
+++ android/src/main/kotlin/com/cloudcare/ft/mobile/sdk/tracker/flutter/FTMobileAgentFlutter.kt
@@ -26,7 +26,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugins.webviewflutter.WebViewFlutterAndroidExternalApi
 
 /** AgentPlugin */
 class FTMobileAgentFlutter : FlutterPlugin, MethodCallHandler, ActivityAware {
